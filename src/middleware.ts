1fe9f13cb14d40b851a9ffa88fbd53d77a6288cf
Aleksandr Shtykov <aashtykov@outlook.com>
2024-10-12 15:49:51 +1000

    auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import type { NextRequest } from "next/server";
+
+/**
+ *
+ * @description Auth middleware. Block access to pages if user is not authenticated.
+ */
+export function middleware(request: NextRequest) {
+  const currentUser = request.cookies.get("auth");
+
+  if (!currentUser && !request.nextUrl.pathname.startsWith("/login")) {
+    return Response.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
