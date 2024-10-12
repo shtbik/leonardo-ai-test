8d283e0058f49b210eb5fedc99895d052096eaf6
Aleksandr Shtykov <aashtykov@outlook.com>
2024-10-12 15:49:38 +1000

    added login api endpoint

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,18 @@
+import { cookies } from "next/headers";
+import type { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  const sessionData = await req.json();
+
+  const cookieStore = cookies();
+  cookieStore.set("auth", JSON.stringify(sessionData), {
+    secure: true,
+    httpOnly: true,
+    sameSite: "strict",
+    maxAge: 60 * 60, // One hour
+  });
+
+  return new Response(null, {
+    status: 200,
+  });
+}
